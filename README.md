d2493cfa247d47097bd6c0592b093d523fa1d23f
Bozz <bozzsrinikorn@gmail.com>
2020-10-19 22:29:33 +0700

    move to model/

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-pytalentsolution
+# Pytalentsolution
+
+Environment Variables
+
+```
+export GOOGLE_APPLICATION_CREDENTIALS=<PATH/TO/CREDENTIALS>
+export PROJECT_ID=<GOOGLE_PROJECT_ID>
+```
--- pytalentsolution/enum_util.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AutoName(Enum):
-    """
-    https://docs.python.org/3/library/enum.html#using-automatic-values
-    """
-    def _generate_next_value_(name, start, count, last_values):
-        return name
