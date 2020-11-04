ddf32dc6b4d6a6783c5be78d5cc49c2764c074b8
Nutchanon Ninyawee <nutchanon@codustry.com>
2020-11-05 01:57:05 +0700

    add model test

---
+++ tests/test_models/test_job.py
@@ -0,0 +1,18 @@
+from pytalentsolution.model.job import CustomAttributes, Job
+
+
+def test_job_with_custom_attribute():
+    job = Job(
+            company="codustry",
+            requisition_id="codustry",
+            title="codustry company",
+            description="a tech company",
+            custom_attributes={
+                "sample": CustomAttributes(
+                        string_values=["hello"],
+                        keyword_searchable=True,
+                )
+            }
+    )
+    job.prepare_for_rpc()
+    print(job)
